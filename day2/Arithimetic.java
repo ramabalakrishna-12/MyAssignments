9c8deef12c2e37678008b7ffdd8ce49efe408c7d
ramabalakrishna-12 <139623955+ramabalakrishna-12@users.noreply.github.com>
2023-07-15 13:15:15 +0200

    Add files via upload

---
+++ day2/Arithimetic.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+import week1.day1.Maths;
+
+public class Arithimetic {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		Maths ar=new Maths();
+		System.out.println("Sum is " +ar.add(1,4));
+		System.out.println("Multiply is " +ar.mul(2, 5));
+		System.out.println("Division is " +ar.div(45, 3));
+		
+	}
+
+}
