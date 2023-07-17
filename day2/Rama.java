97b2fb7a548eae4c2f1b961a02189c486aa3e861
ramabalakrishna-12 <139623955+ramabalakrishna-12@users.noreply.github.com>
2023-07-17 11:26:11 +0200

    Add files via upload

---
+++ day2/Rama.java
@@ -0,0 +1,26 @@
+package week1.day2;
+
+public class Rama {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int mark=75;
+		if(mark==35) {
+			System.out.println("Grade E");
+		}
+		else if(mark>=50) {
+			System.out.println("Grade D");	
+		}
+		else if(mark>65) {
+			System.out.println("Grade C");
+		}
+		else if(mark>=75) {
+			System.out.println("Grade B");
+		}
+		else {
+			System.out.println("Grade A");
+		}
+
+	}
+
+}
